f46028d6be2e9844b7c62246b4ceba978c8ff5d2
Saisha <you@example.com>
2026-04-08 10:30:08 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,28 @@
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
+import math
+
+
 # First example
 def add(a, b): 
-    pass
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return b / a
+    if a == 0:
+        return "Error"
+def log (a, b):
+    return math.log(b, a)
+def exp(a, b):
+    return math.exp(b)
+
 
 
