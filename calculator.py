fe0fd77b17e31932f3eed4dd95fbaf919aa32030
Saisha <you@example.com>
2026-04-08 10:56:06 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -10,20 +10,18 @@ import math
 # First example
 def add(a, b):
     return a + b
-def sub(a, b):
-    return a - b
+
 def mul(a, b):
     return a * b
 def div(a, b):
     if a == 0:
         return "Error"
     return b/a
-def log(a, b):
-    return math.log(a, b)
+
 def exp (a,b):
     return math.exp(a)
 
-import math
+
 
 def square_root(a):
     if a < 0:
@@ -35,18 +33,12 @@ def add(a, b):
     return a + b
 def subtract(a, b):
     return a - b
-def multiply(a, b):
-    return a * b
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    return b / a
+
 def logarithm(a, b):
     if a <= 0 or a == 1:
         raise ValueError
     if b <= 0:
         raise ValueError
     return math.log(b, a)
-def exponent(a, b):
-    return a**b
+
 
